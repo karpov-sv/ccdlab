91aaeb8e49327bacd7c945af80dfb33e3adf32b7
karpov.sv@gmail.com <karpov.sv@gmail.com>
2017-12-16 16:15:08 +0100

    Fix JS for the change of 'clients' from list to dict

--- web/monitor.js
+++ web/monitor.js
@@ -19,10 +19,7 @@ Monitor = function(parent_id, base="/monitor", title="Monitor"){
 
     this.cmdline = $("<input>", {type:"text", size:"40", class:"form-control"});
     this.cmdtarget = $("<select/>", {class:"selectpicker input-group-btn"});
-    //$("<option>", {value: "monitor"}).html("Monitor").appendTo(this.cmdtarget);
-    //$("<option>", {value: "hw"}).html("HW").appendTo(this.cmdtarget);
 
-    //$("<div/>", {class:"input-group"}).append(this.cmdtarget).append($("<span/>", {class:"input-group-addon"}).html("Command:")).append(this.cmdline).appendTo(list);
     $("<div/>", {class:"input-group"}).append($("<span/>", {class:"input-group-addon"}).html("Command:")).append(this.cmdline).appendTo(list);
 
     this.cmdline.pressEnter($.proxy(function(event){
@@ -36,18 +33,7 @@ Monitor = function(parent_id, base="/monitor", title="Monitor"){
     var footer = $("<div/>", {class:"panel-footer"});//.appendTo(panel);
 
     var form = $("<form/>").appendTo(footer);
-    // this.throbber = $("<span/>", {class:"glyphicon glyphicon-transfer"}).appendTo(form);
     this.delayValue = 2000;
-    this.delay = $("<select/>", {id:getUUID()});
-    $("<label>", {for: this.delay.get(0).id}).html("Refresh:").appendTo(form);
-    $("<option>", {value: "1000"}).html("1").appendTo(this.delay);
-    $("<option>", {value: "2000", selected:1}).html("2").appendTo(this.delay);
-    $("<option>", {value: "5000"}).html("5").appendTo(this.delay);
-    $("<option>", {value: "10000"}).html("10").appendTo(this.delay);
-    this.delay.appendTo(form);
-    this.delay.change($.proxy(function(event){
-        this.delayValue = this.delay.val();
-    }, this));
 
     panel.appendTo(parent_id);
 
@@ -65,6 +51,8 @@ Monitor.prototype.requestState = function(){
         success: function(json){
             this.throbber.animate({opacity: 1.0}, 200).animate({opacity: 0.1}, 400);
 
+            this.json = json;
+            
             /* Crude hack to prevent jumping */
             st = document.body.scrollTop;
             sl = document.body.scrollLeft;
@@ -81,11 +69,6 @@ Monitor.prototype.requestState = function(){
 }
 
 Monitor.prototype.updateStatus = function(status, clients){
-    //this.status.html("");
-
-    // status = json.status;
-    // clients = json.clients;
-
     this.last_status = status;
 
     show(this.body);
@@ -93,15 +76,16 @@ Monitor.prototype.updateStatus = function(status, clients){
     this.connstatus.html("Connected");
     this.connstatus.removeClass("label-danger").addClass("label-success");
 
-    if(this.clients.length != clients.length)
+    if(this.clients.length != Object.keys(clients).length){
         this.makeClients(clients);
+    }
 
     state = "";
     //state += "Connections: " + label(status['nconnected']);
     state += " Clients:";
 
-    for(var i=0; i < clients.length; i++){
-        var client = clients[i]
+    for(var i=0; i < this.clients.length; i++){
+        var client = clients[this.clients[i].name];
         var client_status = status[client['name']];
         var html = "";
 
@@ -170,30 +154,26 @@ Monitor.prototype.makeClients = function(clients)
     this.clientsdiv.html("");
     this.clients = [];
     
-    for(var i=0; i < clients.length; i++){
-        this.clients[i] = {};
-
+    for(var name in clients){
+        client = {'name':name};
+        
         var div = $("<div/>", {class:"panel panel-default"}).appendTo(this.clientsdiv);
         var header = $("<div/>", {class:"panel-heading"}).appendTo(div);
         var title = $("<h3/>", {class:"panel-title"}).appendTo(header);
-        this.clients[i].title = $("<span/>", {style:"margin-right: 0.5em"}).html(clients[i]['name']).appendTo(title);
-        this.clients[i].connstatus = $("<span/>", {class:"label label-default", style:"margin-right: 0.5em"}).appendTo(title);
-        this.clients[i].hwstatus = $("<span/>", {class:"label label-default", style:"margin-right: 0.5em"}).appendTo(title);
+        client.title = $("<span/>", {style:"margin-right: 0.5em"}).html(clients[name]['name']).appendTo(title);
+        client.connstatus = $("<span/>", {class:"label label-default", style:"margin-right: 0.5em"}).appendTo(title);
+        client.hwstatus = $("<span/>", {class:"label label-default", style:"margin-right: 0.5em"}).appendTo(title);
         var body = $("<div/>", {class:"panel-body", style:"padding: 1px; margin: 1px"}).appendTo(div);
-        this.clients[i].body = body;
+        client.body = body;
         
-        this.clients[i].progressdiv = $("<div/>", {class:"progress", style:"margin: 0; padding: 0"}).appendTo(body);
-        this.clients[i].progress = $("<div/>", {class:"progress-bar", style:"width: 0%"}).appendTo(this.clients[i].progressdiv);
+        client.progressdiv = $("<div/>", {class:"progress", style:"margin: 0; padding: 0"}).appendTo(body);
+        client.progress = $("<div/>", {class:"progress-bar", style:"width: 0%"}).appendTo(client.progressdiv);
 
-        this.clients[i].state = $("<div/>", {class: "", style:"padding: 5px"}).appendTo(body);
+        client.state = $("<div/>", {class: "", style:"padding: 5px"}).appendTo(body);
 
-        //var list = $("<ul/>", {class:"list-group"}).appendTo(div);
-        //this.clients[i].list = list;
-        //this.clients[i].state = $("<li/>", {class: "list-group-item", style:"padding: 5px"}).appendTo(list);
+        if(clients[name]['description'])
+            client.title.html(clients[name]['description']);
 
-        //$("<option>", {value: this.clients[i]['name']}).html(this.clients[i]['name']).appendTo(this.cmdtarget);
-
-        if(clients[i]['description'])
-            this.clients[i].title.html(clients[i]['description']);
+        this.clients.push(client);
     }
 }
