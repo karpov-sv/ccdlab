3b45e3da767414fb4818c2543332d96dd87246ec
karpov.sv@gmail.com <karpov.sv@gmail.com>
2017-12-16 02:43:43 +0100

    Now we know how to plot time histories for given variables

--- monitor.py
+++ monitor.py
@@ -7,10 +7,14 @@ from twisted.web.resource import Resource
 from twisted.web.static import File
 from twisted.internet.endpoints import TCP4ServerEndpoint
 
-import os, sys, posixpath
+import os, sys, posixpath, datetime
 import re
 import urlparse
 import json
+
+from StringIO import StringIO
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 from daemon import SimpleFactory, SimpleProtocol
 from command import Command
@@ -37,7 +41,7 @@ class MonitorProtocol(SimpleProtocol):
         SimpleProtocol.__init__(self)
         self.name = None
         self.status = {}
-
+        
     def connectionMade(self):
         SimpleProtocol.connectionMade(self)
 
@@ -58,9 +62,19 @@ class MonitorProtocol(SimpleProtocol):
             # We keep var=value pairs from the status to report it to clients
             self.status = cmd.kwargs
 
+            # We have to keep the history of values for some variables for plots
+            if self.object['values'].has_key(self.name):
+                for name in self.object['values'][self.name]:
+                    if name == 'time':
+                        value = datetime.datetime.utcnow()
+                    else:
+                        value = self.status.get(name, None)
+                        
+                    self.object['values'][self.name][name].append(value)
+            
         elif cmd.name == 'get_status':
             self.message(self.factory.getStatus())
-
+        
     def update(self):
         self.factory.messageAll('get_status')
 
@@ -73,7 +87,7 @@ class MonitorFactory(SimpleFactory):
             status = 'status nconnected=%d' % len(self.connections)
 
         # Monitor only specified connections
-        for name in [_['name'] for _ in self.object['clients'] if _['name']]:
+        for name in self.object['clients']:
             c = self.findConnection(name=name)
             if c:
                 if as_dict:
@@ -121,7 +135,7 @@ class CmdlineProtocol(LineReceiver):
 
         elif cmd.name == 'clients':
             print "Number of registered clients:", len(self.object['clients'])
-            for c in self.object['clients']:
+            for name,c in self.object['clients'].items():
                 conn = self.factory.findConnection(name=c['name'])
                 self.message("  %s:%s name:%s connected:%s" % (c['host'], c['port'], c['name'], conn!=None))
             self.message()
@@ -140,6 +154,34 @@ def serve_json(request, **kwargs):
     request.responseHeaders.setRawHeaders("Content-Type", ['application/json'])
     return json.dumps(kwargs)
 
+def make_plot(file, obj, client_name, plot_name, size=800):
+    plot = obj['clients'][client_name]['plots'][plot_name]
+    values = obj['values'][client_name]
+
+    fig = Figure(facecolor='white', dpi=72, figsize=(plot['width']/72, plot['height']/72), tight_layout=True)
+    ax = fig.add_subplot(111)
+
+    for _ in plot['values'][1:]:
+        ax.plot(values[plot['values'][0]], values[_], '.-', label=_)
+
+    if plot['xlabel']:
+        ax.set_xlabel(plot['xlabel'])
+    else:
+        ax.set_xlabel(plot['values'][0])
+
+    if plot['ylabel']:
+        ax.set_ylabel(plot['ylabel'])
+    elif len(plot['values']) > 2:
+        ax.legend(frameon=False)
+    else:
+        ax.set_ylabel(plot['values'][1])
+
+    ax.set_title(plot['name'])
+        
+    # Return the image
+    canvas = FigureCanvas(fig)
+    canvas.print_png(file, bbox_inches='tight')    
+    
 class WebMonitor(Resource):
     isLeaf = True
 
@@ -147,14 +189,23 @@ class WebMonitor(Resource):
         self.factory = factory
         self.object = object
 
+    @catch
     def render_GET(self, request):
         q = urlparse.urlparse(request.uri)
         args = urlparse.parse_qs(q.query)
+        qs = q.path.split('/')
 
         if q.path == '/monitor/status':
             return serve_json(request,
                               clients = self.object['clients'],
                               status = self.factory.getStatus(as_dict=True))
+        elif qs[1] == 'monitor' and qs[2] == 'plot' and len(qs) > 4:
+            s = StringIO()
+            make_plot(s, self.object, qs[3], qs[4])
+            request.responseHeaders.setRawHeaders("Content-Type", ['image/png'])
+            request.responseHeaders.setRawHeaders("Content-Length", [s.len])
+            request.responseHeaders.setRawHeaders("Cache-Control", ['no-store, no-cache, must-revalidate, max-age=0'])
+            return s.getvalue()
         elif q.path == '/monitor/command':
             cmd = Command(args['string'][0])
 
@@ -171,27 +222,30 @@ class WebMonitor(Resource):
 
             return serve_json(request)
 
-if __name__ == '__main__':
-    from optparse import OptionParser
+def loadINI(filename, obj):
+    # We use ConfigObj library from http://www.voidspace.org.uk/python/configobj.html
     from configobj import ConfigObj,Section # apt-get install python-configobj
     from validate import Validator
-    from StringIO import StringIO
-    
-    # Object holding actual state and work logic.
-    obj = {'clients':[], 'port':7100, 'name':'monitor'}
-
-    # First read client config from INI file
-    # We use ConfigObj library from http://www.voidspace.org.uk/python/configobj.html
 
     # Schema to validate and transform the values from config file
     schema = ConfigObj(StringIO('''
     port = integer(min=0,max=65535,default=%d)
     name = string(default=%s)
+
     [__many__]
     enabled = boolean(default=True)
     port = integer(min=0,max=65535,default=0)
     host = string(default="localhost")
     description = string(default=None)
+
+    [[plots]]
+    [[[__many__]]]
+    name = string(default=None)
+    values = list(default=,)
+    xlabel = string(default=None)
+    ylabel = string(default=None)
+    width = integer(min=0,max=2048,default=800)
+    height = integer(min=0,max=2048,default=300)
     ''' % (obj['port'], obj['name'])), list_values=False)
 
     confname = '%s.ini' % posixpath.splitext(__file__)[0]
@@ -201,22 +255,53 @@ if __name__ == '__main__':
         if result != True:
             print "Config file failed validation: %s" % confname
             print result
-            sys.exit(1)
 
-        for section in conf:
+            raise RuntimeError
+
+        for sname in conf:
+            section = conf[sname]
+            
             # Skip leafs and branches with enabled=False
-            if type(conf[section]) != Section or not conf[section]['enabled']:
+            if type(section) != Section or not section['enabled']:
                 continue
 
-            client = {'name':section,
-                      'host':conf[section]['host'],
-                      'port':conf[section]['port'],
-                      'description':conf[section]['description']}
+            client = {'name':sname,
+                      'host':section['host'],
+                      'port':section['port'],
+                      'description':section['description'],
+                      'plots':{}}
 
-            obj['clients'].append(client)
+            obj['values'][sname] = {}
+
+            if section.has_key('plots'):
+                values = []
+                
+                # Parse parameters of plots
+                for plot in section['plots']:
+                    client['plots'][plot] = section['plots'][plot]
+
+                    values += section['plots'][plot]['values']
+                
+                obj['values'][sname] = {_:[] for _ in set(values)} # Unique values
+                
+            obj['clients'][sname] = client
 
         obj['port'] = conf.get('port')
         obj['name'] = conf.get('name')
+
+    # print obj
+    # sys.exit(1)
+        
+    return True
+        
+if __name__ == '__main__':
+    from optparse import OptionParser
+    
+    # Object holding actual state and work logic.
+    obj = {'clients':{}, 'values':{}, 'port':7100, 'name':'monitor'}
+
+    # First read client config from INI file
+    loadINI('%s.ini' % posixpath.splitext(__file__)[0], obj)
 
     # Now parse command-line arguments using values read from config as defaults
     # so that they may be changed at startup time
@@ -231,21 +316,18 @@ if __name__ == '__main__':
         m = re.match('(([a-zA-Z0-9-_]+)=)?(.*):(\d+)', arg)
         if m:
             name,host,port = m.group(2,3,4)
-            print name,host,port
 
-            client = [_ for _ in obj['clients'] if _['name'] == name]
-
-            if client:
-                client[0]['host'] = host
-                client[0]['port'] = int(port)
+            if obj['clients'].has_key(name):
+                obj['clients'][name]['host'] = host
+                obj['clients'][name]['port'] = int(port)
             else:
-                obj['clients'].append({'host':host, 'port':int(port), 'name':name, 'description':None})
+                obj['clients'][name] = {'host':host, 'port':int(port), 'name':name, 'description':None}
 
-    # Now we have everything to construct an run the daemon
+    # Now we have everything to construct and run the daemon
     daemon = MonitorFactory(MonitorProtocol, obj, name=options.name)
     daemon.listen(options.port)
 
-    for c in obj['clients']:
+    for name,c in obj['clients'].items():
         daemon.connect(c['host'], c['port'])
 
     # Simple stdio interface
