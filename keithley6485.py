eddab07c72e6b3dc850a4e78021b70287bb1df7d
Asen Christov <asen.christov@gmail.com>
2018-02-02 16:09:36 +0100

    gpib.py work, k6485 too, the rest is work in progress

--- keithley6485.py
+++ keithley6485.py
@@ -7,7 +7,7 @@ from command import Command
 from daemon import catch
 
 class DaemonProtocol(SimpleProtocol):
-    _debug = False # Display all traffic for debug purposes
+    _debug = True # Display all traffic for debug purposes
 
     @catch
     def processMessage(self, string):
@@ -37,7 +37,7 @@ class DaemonProtocol(SimpleProtocol):
         hw.messageAll(string, type='hw', keep=keep, source=self.name)
 
 class KeithleyProtocol(SimpleProtocol):
-    _debug = False # Display all traffic for debug purposes
+    _debug = True # Display all traffic for debug purposes
     _refresh = 0.1
 
     def __init__(self):
@@ -53,7 +53,7 @@ class KeithleyProtocol(SimpleProtocol):
         SimpleProtocol.connectionMade(self)
         self.object['hw_connected'] = 0 # We will set this flag when we receive any reply from the device
         SimpleProtocol.message(self, 'set_addr %d' % self.object['addr'])
-        SimpleProtocol.message(self, '*opc?')
+        SimpleProtocol.message(self, '?$*opc?')
        
     @catch
     def connectionLost(self, reason):
@@ -105,7 +105,11 @@ class KeithleyProtocol(SimpleProtocol):
         internal queue so that we may properly recognize the reply
         """
         cmd = Command(string)
-        self.commands.append({'cmd':cmd.name,'source':source,'timeStamp':datetime.datetime.utcnow(),'keep':keep})
+        if keep:
+            SimpleProtocol.message(self, '?$%s' % cmd.name)
+            self.commands.append({'cmd':cmd.name,'source':source,'timeStamp':datetime.datetime.utcnow(),'keep':keep})
+        else:
+            SimpleProtocol.message(self, string.name)
 
     @catch
     def update(self):
@@ -119,38 +123,8 @@ class KeithleyProtocol(SimpleProtocol):
             #if not connected do not send any commands
             return
 
-        if self._debug: print len(self.commands),' commands in the Q:\n',self.commands, "waitForMessage= ",self.waitForMessage
-        
-        if len(self.commands):
-            if self.commands[0]['cmd']=='*opc?' and self.commands[0]['source']=='itself' and not self.waitForMessage:
-                if self._debug: print 'last command was auto *opc? call, sorting out the result'
-                if self.commands[0]['keep']:
-                    #check opc status, because last opc status indicated divice is busy send it again, no need to change the Q
-                    if self._debug: print '*opc? waiting'
-                    SimpleProtocol.message(self, '%s' % ('*opc?'))
-                elif not self.commands[0]['keep']:
-                    # opc status is 1, the device is ready, send next command
-                    self.commands.pop(0) #remove the opc from queue
-                    if self._debug: print '*opc? indicates device ready, sending command ',self.commands[0]['cmd']
-                    SimpleProtocol.message(self, '%s' % (self.commands[0]['cmd'])) #send the actual command
-                    if not self.commands[0]['keep']:
-                        self.commands.pop(0)
-                    else:
-                        self.waitForMessage=True   
-            else:
-                if not self.waitForMessage:
-                    #before sending a new command, send opc query
-                    if self._debug: print '*opc? before next command'
-                    self.commands.insert(0,{'cmd':'*opc?','source':'itself','timeStamp':datetime.datetime.utcnow(),'keep':True})
-                    SimpleProtocol.message(self, '%s' % ('*opc?'))
-                elif not self.commands[0]['keep']:
-                    #the last command is not periodic *opc? check, it was actual command expecting answer and the answer arrived
-                    if self._debug: print 'it was actual command expecting answer and the answer arrived'
-                    self.commands.pop(0) #remove the actual command from the Q
-                    self.waitForMessage=False
         elif (datetime.datetime.utcnow()-self.lastAutoRead).total_seconds()>2.:
             # Request the hardware state from the device
-            self.message('*rst', keep=False, source='itself')
             self.message('read?', keep=True, source='itself')
             self.lastAutoRead=datetime.datetime.utcnow()
 
