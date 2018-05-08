11c12ad66311599344d95ea3f4fe39522e47d6f2
Sergey Karpov <karpov@panther.local>
2018-05-09 01:45:51 +0200

    Small adjustments for Pfeiffer

--- pfeifferLAN.py
+++ pfeifferLAN.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os, sys
+import numpy as np
 
 from daemon import SimpleFactory, SimpleProtocol
 from command import Command
@@ -10,6 +11,7 @@ from daemon import catch
 
 class DaemonProtocol(SimpleProtocol):
     _debug = False # Display all traffic for debug purposes
+    _simulator = False
 
     @catch
     def processMessage(self, string):
@@ -19,7 +21,10 @@ class DaemonProtocol(SimpleProtocol):
         hw = obj['hw'] # HW factory
 
         if cmd.name == 'get_status':
-            self.message('status hw_connected=%s status=%d pressure=%g' % (self.object['hw_connected'], self.object['status'], self.object['pressure']))
+            if self._simulator:
+                self.message('status hw_connected=1 status=0 pressure=%g simulator=1' % (np.random.uniform(1.0, 10.0)))
+            else:
+                self.message('status hw_connected=%s status=%d pressure=%g' % (self.object['hw_connected'], self.object['status'], self.object['pressure']))
         else:
             if obj['hw_connected']:
                 # Pass all other commands directly to hardware
@@ -50,7 +55,7 @@ class HWProtocol(SimpleProtocol):
             # b,sx.xxxxEsxx
             self.object['status'] = int(string[0])
             self.object['pressure'] = float(string[2:])
-            
+
     @catch
     def message(self, string):
         """Sending outgoing message"""
@@ -59,7 +64,7 @@ class HWProtocol(SimpleProtocol):
         self.transport.write(string)
         self.transport.write("\r\n")
 
-            
+
     @catch
     def update(self):
         # Request the hardware state from the device
@@ -71,15 +76,16 @@ if __name__ == '__main__':
     parser = OptionParser(usage="usage: %prog [options] arg")
     parser.add_option('-H', '--hw-host', help='Hardware host to connect', action='store', dest='hw_host', default='192.168.1.11')
     parser.add_option('-P', '--hw-port', help='Hardware port to connect', action='store', dest='hw_port', type='int', default=8000)
-    parser.add_option('-p', '--port', help='Daemon port', action='store', dest='port', type='int', default=7024)
-    parser.add_option('-n', '--name', help='Daemon name', action='store', dest='name', default='pfeifferLAN')
+    parser.add_option('-p', '--port', help='Daemon port', action='store', dest='port', type='int', default=7023)
+    parser.add_option('-n', '--name', help='Daemon name', action='store', dest='name', default='pfeiffer')
     parser.add_option("-D", '--debug', help='Debug mode', action="store_true", dest="debug")
+    parser.add_option("-S", '--simulator', help='Simulator mode', action="store_true", dest="simulator")
 
     (options,args) = parser.parse_args()
-   
+
     # Object holding actual state and work logic.
     # May be anything that will be passed by reference - list, dict, object etc
-    obj = {'hw_connected':0}
+    obj = {'hw_connected':0, 'status':-1, 'pressure':0}
 
     # Factories for daemon and hardware connections
     # We need two different factories as the protocols are different
@@ -90,10 +96,13 @@ if __name__ == '__main__':
 
     obj['daemon'] = daemon
     obj['hw'] = hw
-    
+
     if options.debug:
-        daemon._protocol._debug=True
-        hw._protocol._debug=True
+        daemon._protocol._debug = True
+        hw._protocol._debug = True
+
+    if options.simulator:
+        daemon._protocol._simulator = True
 
     # Incoming connections
     daemon.listen(options.port)
