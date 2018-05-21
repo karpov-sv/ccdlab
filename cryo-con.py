d87e3c018bccd6aafde1bb8229f7ef992ea57011
Asen Christov <asen.christov@gmail.com>
2018-05-21 15:16:09 +0200

    cryo-con heater monitoring

--- cryo-con.py
+++ cryo-con.py
@@ -23,9 +23,13 @@ class DaemonProtocol(SimpleProtocol):
                 self.message('status hw_connected=1 status=0 temperatureA=%g  temperatureB=%g temperatureC=%g temperatureD=%g simulator=1'
                              % (np.random.uniform(1.0, 10.0),np.random.uniform(1.0, 10.0),np.random.uniform(1.0, 10.0),np.random.uniform(1.0, 10.0)))
             else:
-                self.message('status hw_connected=%s status=%s temperatureA=%g temperatureB=%g temperatureC=%g temperatureD=%g'
+                self.message('status hw_connected=%s status=%s temperatureA=%g temperatureB=%g temperatureC=%g temperatureD=%g \
+                             htr_status1=%s range1=%s ctrl_type1=%s pwr_set1=%g pwr_actual1=%g load1=%g \
+                             htr_status2=%s range2=%s ctrl_type2=%s pwr_set2=%g pwr_actual2=%g load2=%g'
                              % (self.object['hw_connected'], self.object['status'],
-                                self.object['temperatureA'], self.object['temperatureB'], self.object['temperatureC'], self.object['temperatureD']))
+                                self.object['temperatureA'], self.object['temperatureB'], self.object['temperatureC'], self.object['temperatureD'],
+                                self.object['htr_status1'], self.object['range1'], self.object['ctrl_type1'], self.object['pwr_set1'], self.object['pwr_actual1'],self.object['load1'],
+                                self.object['htr_status2'], self.object['range2'], self.object['ctrl_type2'], self.object['pwr_set2'], self.object['pwr_actual2'],self.object['load1']))
         else:
             if obj['hw_connected']:
                 # Pass all other commands directly to hardware
@@ -44,7 +48,7 @@ class HWProtocol(SimpleProtocol):
         self.type = 'hw'
         SimpleProtocol.connectionMade(self)
         # make sure the units are Celsius
-        self.message('input a,b,c,d:units c')
+        self.message('input a,b,c,d:units c;:loop 1:load 50')
 
     @catch
     def connectionLost(self, reason):
@@ -57,14 +61,19 @@ class HWProtocol(SimpleProtocol):
         # Process the device reply
         if self._debug:
             print "hw > %s" % string
-
+            
+        pwrfactor = {'HI':1.,'MID':0.1,'LOW':0.01}
+        
         if len(string):
+            # reply to parse looks like this:
+            # 20.806670;20.800480;20.896670;20.853670;--Htr OK--;HI ;MAN  ;50;0.000000;   0%;0.000A; 0.00V;--Htr OK--;LOW;MAN  ;50;0.000000;   0%;0.000A; 0.00V
             # values for channel a;b;c;d (....... means dot connected)
             sstring = string.split(';')
             status = ''
             channel = ['temperatureA', 'temperatureB', 'temperatureC', 'temperatureD']
 
-            for s in range(len(sstring)):
+            # temperatures
+            for s in range(4):
                 try:
                     sstring[s] = float(sstring[s])
                     status = status + '1'
@@ -73,12 +82,27 @@ class HWProtocol(SimpleProtocol):
                     status = status + '0'
                     self.object[channel[s]] = np.nan
             self.object['status'] = status
+            # heater loop 1
+            self.object['htr_status1'] = sstring[4].replace(' ','-')
+            self.object['range1'] = sstring[5].replace(' ','')
+            self.object['ctrl_type1'] = sstring[6].replace(' ','')
+            self.object['load1'] = float(sstring[7])
+            self.object['pwr_set1'] = float(sstring[8])*pwrfactor[self.object['range1']]*self.object['load1']/100.
+            self.object['pwr_actual1'] = float(sstring[9].replace('%',''))*pwrfactor[self.object['range1']]*self.object['load1']/100.
+            self.object['htr_status2'] = sstring[10].replace(' ','-')
+            self.object['range2'] = sstring[11].replace(' ','')
+            self.object['ctrl_type1'] = sstring[12].replace(' ','')
+            self.object['load2'] = float(sstring[13])
+            self.object['pwr_set2'] = float(sstring[14])*pwrfactor[self.object['range2']]*self.object['load2']*0.5/100.
+            self.object['pwr_actual2'] = float(sstring[15].replace('%',''))*pwrfactor[self.object['range2']]*self.object['load2']*0.5/100.
+            
+            
 
     @catch
     def update(self):
         # Request the hardware state from the device
-        self.message('input? a,b,c,d')
-
+        #self.message('input? a,b,c,d')
+        self.message('input? a,b,c,d;:loop 1:err?;rang?;type?;load?;outp?;htrr?;:loop 2:err?;rang?;type?;load?;outp?;htrr?;')
 if __name__ == '__main__':
     from optparse import OptionParser
 
@@ -94,7 +118,10 @@ if __name__ == '__main__':
 
     # Object holding actual state and work logic.
     # May be anything that will be passed by reference - list, dict, object etc
-    obj = {'hw_connected':0, 'status':'----', 'temperatureA':0, 'temperatureB':0, 'temperatureC':0, 'temperatureD':0}
+    obj = {'hw_connected':0, 
+           'status':'----', 'temperatureA':0, 'temperatureB':0, 'temperatureC':0, 'temperatureD':0,
+           'htr_status1':'-','range1':'-','ctrl_type1':'-','pwr_set1':0,'pwr_actual1':0,'load1':0,'current1':0,
+           'htr_status2':'-','range2':'-','ctrl_type2':'-','pwr_set2':0,'pwr_actual2':0,'load2':0,'current2':0}
 
     # Factories for daemon and hardware connections
     # We need two different factories as the protocols are different
