79fe55528e3da08cecdf481ab777eb1619642de8
Sergey Karpov <karpov@panther.local>
2018-06-01 04:19:04 +0200

    Interactive popup plots for browsing status history

--- archive/views_status.py
+++ archive/views_status.py
@@ -17,7 +17,16 @@ from models import MonitorStatus
 
 def parse_time(string):
     # FIXME: parse more time formats!
-    return datetime.datetime.strptime(string, "%Y.%m.%d %H:%M:%S")
+
+    for fmt in ["%Y.%m.%d %H:%M:%S", "%Y-%m-%d %H:%M:%S"]:
+        try:
+            time = datetime.datetime.strptime(string, fmt)
+            return time
+        except ValueError:
+            pass
+
+    print "Can't parse time string:", string
+    return None
 
 def status(request):
     context = {}
@@ -45,11 +54,10 @@ def status(request):
 
 def status_plot(request, params, width=1000.0, height=500.0, hours=24.0, title=None, xlabel="Time, UT", ylabel=None, ylog=False, grid=True):
     hours = float(hours) if hours else 24.0
-    # delay = int(delay) if delay else 0
 
-    # # Strip all but good characters
-    # spath = re.sub('[^0-9a-zA-Z_.]+', '', path)
-    # spath = '\'' + path + '\''
+    time0 = None # Mid-time for 'zooming' plot
+
+    time1,time2 = None,None
 
     if request.GET:
         width = float(request.GET.get('width', width))
@@ -63,8 +71,25 @@ def status_plot(request, params, width=1000.0, height=500.0, hours=24.0, title=N
         xlabel = request.GET.get('xlabel', xlabel)
         ylabel = request.GET.get('ylabel', ylabel)
 
+        # Time range
+        if request.GET.has_key('time0'):
+            time0 = parse_time(request.GET.get('time0'))
+
+            time1 = time0 - datetime.timedelta(hours=hours/2)
+            time2 = time0 + datetime.timedelta(hours=hours/2)
+
+    if time1 is None or time2 is None:
+        # Default time range is given number of hours back from now
+        time1 = datetime.datetime.utcnow() - datetime.timedelta(hours=hours)
+        time2 = datetime.datetime.utcnow()
+
     if not title:
         title = params
+
+        if time0:
+            title += ' : ' + str(hours) + ' hours around ' + time0.strftime('%Y.%m.%d %H:%M:%S') + ' UT'
+        else:
+            title += ' : ' + time1.strftime('%Y.%m.%d %H:%M:%S') + ' UT - ' + time2.strftime('%Y.%m.%d %H:%M:%S') + ' UT'
 
     # Parse comma-separated list of client.param strings
     # TODO: add support for root level parameters, with no dots
@@ -72,7 +97,7 @@ def status_plot(request, params, width=1000.0, height=500.0, hours=24.0, title=N
     select = {}
 
     if not ylabel and len(params) == 1:
-        ylabel = params
+        ylabel = params[0]
 
     labels = []
     for param in params:
@@ -81,9 +106,8 @@ def status_plot(request, params, width=1000.0, height=500.0, hours=24.0, title=N
         labels.append(s[0]+'.'+s[1])
 
     ms = MonitorStatus.objects.extra(select=select).defer('status').order_by('time')
-    ms = ms.filter(time__gt = datetime.datetime.utcnow() - datetime.timedelta(hours=hours))
-
-    print datetime.datetime.utcnow() - datetime.timedelta(hours=hours)
+    ms = ms.filter(time__gt = time1)
+    ms = ms.filter(time__lte = time2)
 
     values = [[getattr(_,__) for _ in ms] for __ in labels]
     time = [_.time for _ in ms]
@@ -100,15 +124,21 @@ def status_plot(request, params, width=1000.0, height=500.0, hours=24.0, title=N
             has_data = True
             ax.plot(time, value, '-', label=labels[_].split('.')[-1])
 
-    if time and has_data: # It is failing if no data are plotted
-        if (time[-1] - time[0]).total_seconds() < 2*24*3600:
-            ax.xaxis.set_major_formatter(DateFormatter('%H:%M:%S'))
-        elif (time[-1] - time[0]).total_seconds() > 3*24*3600:
-            ax.xaxis.set_major_formatter(DateFormatter('%Y.%m.%d'))
-        else:
-            ax.xaxis.set_major_formatter(DateFormatter('%Y.%m.%d %H:%M:%S'))
+    # if time and has_data: # It is failing if no data are plotted
+    if (time2 - time1).total_seconds() < 2*24*3600:
+        ax.xaxis.set_major_formatter(DateFormatter('%H:%M:%S'))
+    elif (time2 - time1).total_seconds() > 3*24*3600:
+        ax.xaxis.set_major_formatter(DateFormatter('%Y.%m.%d'))
+    else:
+        ax.xaxis.set_major_formatter(DateFormatter('%Y.%m.%d %H:%M:%S'))
 
-        fig.autofmt_xdate()
+    fig.autofmt_xdate()
+
+    if request.GET and request.GET.has_key('mark'):
+        time_mark = parse_time(request.GET.get('mark'))
+        ax.axvline(time_mark, color='red', ls='--', alpha=1.0)
+
+    ax.set_xlim(time1, time2)
 
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
