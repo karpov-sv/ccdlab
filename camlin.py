845bab3b8dca13a432fcdba86d114f7eb96f20b4
asen.christov@gmail.com <asen.christov@gmail.com>
2020-07-02 10:47:31 +0200

    init function for camplin

--- camlin.py
+++ camlin.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
-import os, sys
+import os
+import sys
 
 from daemon import SimpleFactory, SimpleProtocol, catch
 from command import Command
 
 from camlinlib import MonoChromator, GetPortAndPaths
 
-### Camlin code with server daemon and outgoing connection to hardware
+# Camlin code with server daemon and outgoing connection to hardware
+
 
 class DaemonProtocol(SimpleProtocol):
-    _debug = False # Display all traffic for debug purposes
-    lampDict={'Xe':0,'W':1,0:'Xe',1:'W'}
+    _debug = False  # Display all traffic for debug purposes
+    lampDict = {'Xe': 0, 'W': 1, 0: 'Xe', 1: 'W'}
 
     @catch
     def processMessage(self, string):
@@ -19,17 +21,16 @@ class DaemonProtocol(SimpleProtocol):
         cmd = SimpleProtocol.processMessage(self, string)
         if cmd is None:
             return
-        
-        obj = self.object # Object holding the state
-        hw = obj['hw'] # HW factory
 
+        obj = self.object  # Object holding the state
+        hw = obj['hw']  # HW factory
         if cmd.name == 'get_status':
             keys = ['hw_connected', 'lamp', 'wavelength', 'grating', 'grooves', 'blaze', 'filter', 'shutter', 'autofilter']
 
             self.message('status ' + ' '.join([_+'='+str(obj.get(_)) for _ in keys]))
-
         elif obj['hw_connected']:
             # Accept commands only when HW is connected
+
             if cmd.name == 'set':
                 filter = int(cmd.get('filter', obj.get('filter')))
                 shutter = int(cmd.get('shutter', obj.get('shutter')))
@@ -49,13 +50,13 @@ class DaemonProtocol(SimpleProtocol):
                     if obj['autofilter']:
                         filters = [None, 395, 695, 1000, None, None]
                         filter = 1
-                        for _,__ in enumerate(filters):
+                        for _, __ in enumerate(filters):
                             if __ and __ < wavelength:
                                 filter = _ + 1
-                                
+
                 if lamp != obj.get('lamp'):
                     self.factory.log('Swithing to '+lamp+' lamp')
-                    if hw.set_mirror_position(1,self.lampDict[lamp]) != 0:
+                    if hw.set_mirror_position(1, self.lampDict[lamp]) != 0:
                         self.factory.log(hw.GetErrorName(hw.result))
 
                 if filter != obj.get('filter'):
@@ -67,6 +68,19 @@ class DaemonProtocol(SimpleProtocol):
                     self.factory.log('Setting shutter to %d' % shutter)
                     if (shutter and hw.open_shutter(obj['hw_shutter']) != 0) or (not shutter and hw.close_shutter(obj['hw_shutter']) != 0):
                         self.factory.log(hw.GetErrorName(hw.result))
+            if string.startswith('init '):
+                tti = string.split()[1]
+                num = -1
+                if tti == 'turret':
+                    num = 1
+                elif tti == 'filter':
+                    num = 3
+                elif tti == 'mirror':
+                    num = 4
+                if num != -1:
+                    hw.initialise_device(num=num)
+                else:
+                    self.factory.log('unknown device to init: '+tti, type='info')
 
     @catch
     def update(self):
@@ -102,6 +116,7 @@ class DaemonProtocol(SimpleProtocol):
             if obj['grooves'] is None:
                 obj['hw_connected'] = 0
 
+
 if __name__ == '__main__':
     from optparse import OptionParser
 
@@ -110,18 +125,18 @@ if __name__ == '__main__':
     parser.add_option('-p', '--port', help='Daemon port', action='store', dest='port', type='int', default=7025)
     parser.add_option('-n', '--name', help='Daemon name', action='store', dest='name', default='camlin')
     parser.add_option("-D", '--debug', help='Debug mode', action="store_true", dest="debug")
-    (options,args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
     # Object holding actual state and work logic.
     # May be anything that will be passed by reference - list, dict, object etc
-    obj = {'hw_connected':0, 'autofilter':1}
+    obj = {'hw_connected': 0, 'autofilter': 1}
 
     # Factory for daemon connections
     daemon = SimpleFactory(DaemonProtocol, obj)
     daemon.name = options.name
 
     # Hardware
-    device,libpath,calibpath = GetPortAndPaths()
+    device, libpath, calibpath = GetPortAndPaths()
     if options.device:
         device = options.device
 
