7664b4f533c3d615bb8bda181c801b53ef046496
asenchristov <asen.christov@gmail.com>
2024-03-27 13:25:10 +0100

    Update archive for newer Python and Django

--- archive/urls.py
+++ archive/urls.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf.urls import include, url
+from django.urls import include, re_path
 
 try:
     import settings
@@ -11,24 +11,24 @@ except:
 
 urlpatterns = [
     # Index
-    url(r'index^$', views.index, name="index"),
-    url(r'^$', views.current, name="current"),
+    re_path(r'index^$', views.index, name="index"),
+    re_path(r'^$', views.current, name="current"),
 
     # Log view
-    url(r'^logs(/(?P<source>[a-zA-Z0-9_\-/.,]+)?)?$', views.logs_list, name='logs'),
+    re_path(r'^logs(/(?P<source>[a-zA-Z0-9_\-/.,]+)?)?$', views.logs_list, name='logs'),
 
     # Status
-    url(r'^status/?$', views_status.status, name='status'),
-    url(r'^status/plots/(?P<params>[a-zA-Z0-9_\-/.,]+)/?$', views_status.status_plot, name='status_plot'),
+    re_path(r'^status/?$', views_status.status, name='status'),
+    re_path(r'^status/plots/(?P<params>[a-zA-Z0-9_\-/.,]+)/?$', views_status.status_plot, name='status_plot'),
 
     # Robots
-    url(r'^robots.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /\n", content_type="text/plain")),
+    re_path(r'^robots.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /\n", content_type="text/plain")),
 
     # Markdown
     #url(r'^about/(?P<path>.*)$', views_markdown.markdown_page, {'base':'about'}, name="markdown"),
 
     # MONITOR proxy
-    url(r'^monitor/?$', views.monitor, name="monitor"),
+    re_path(r'^monitor/?$', views.monitor, name="monitor"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
